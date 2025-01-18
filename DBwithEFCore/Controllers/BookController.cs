802a0803c0b18f80af5be57fe7f40b99c4ac8721
M.Suleman <muhammadsulemanm431@gmail.com>
2025-01-18 18:48:41 +0500

    Inserting Record in Database

---
+++ DBwithEFCore/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using DBwithEFCore.Data;
+using DBwithEFCore.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBwithEFCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController(AppDbContext appDbContext) : ControllerBase   // using primary construct a shortcut way for dependency injection
+    {
+        [HttpPost("")]
+        public async Task<IActionResult> AddNewBook([FromBody] Book model)
+        {
+            appDbContext.Books.Add(model);
+            await appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+    }
+}
